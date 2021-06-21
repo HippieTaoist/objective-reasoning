2b6511bd671e08649aa29eb569e9f2256cda5e77
[HippieTaoist] <[shawn.m.kittel@gmail.com]>
2021-06-21 12:28:32 -0600

    finished 5 of 6

--- main.js
+++ main.js
@@ -1,6 +1,67 @@
 // ┌─────────────────┐
 // │ Your code here! │	
 // └─────────────────┘
+console.log('Objective-Reasoning')
+
+//isAdmin
+// Check if user is ADMIN or not.
+// Return true|false value
+function isAdmin(user) {
+
+    // check user role key for ADMIN status | Return
+    return user.userRole === 'ADMIN'
+}
+
+
+//getEmail
+//Return email
+function getEmail(user) {
+    // Create user email key | all lowercase: 
+    // firstname.lastname@CodeImmersives.com
+    user.email = user.firstName.toLowerCase() + '.' + user.lastName.toLowerCase() + '@codeimmersives.com';
+
+
+    // Return user email key
+    return user.email;
+}
+
+
+//  getPlayistLength
+// Return number of songs in received playlist
+function getPlaylistLength(playlist) {
+    // Return number of items in the array
+    // playlist.songs key
+    return playlist.songs.length;
+}
+
+
+// getHardestHomework([])
+// Return the lowest graded key for assignments
+function getHardestHomework(homework) {
+    if (homework.length === 0) {
+        return ''
+    }
+    let lowestScore = 100;
+    for (let i = 0; i < homework.length; i++) {
+        const tempSum = homework[i].averageScore;
+        if (tempSum < lowestScore) {
+            lowestScore = tempSum;
+        }
+
+
+
+    }
+    for (let i = 0; i < homework.length; i++) {
+        const toughestWork = homework[i];
+        if (toughestWork.averageScore === lowestScore) { return toughestWork.name }
+
+    }
+    if (homework.averageScore === lowestScore) {
+        return homework.name;
+
+    }
+    console.log(lowestScore)
+}
 
 
 
